8fb95efabdbf85f25b2bd20ca8ff844f5f6ad55e
Mark D 862 <u862@intrafile.com>
2015-09-10 13:21:19 -0700

    Move key handling and world loading / creation to WorldDisplay

--- WorldMapper/WorldDisplay.java
+++ WorldMapper/WorldDisplay.java
@@ -1,7 +1,9 @@
+import java.lang.Object;
+import java.io.*;
 import processing.core.*;
 import java.awt.Rectangle;
 
-class WorldDisplay {
+public class WorldDisplay {
   PApplet _applet;
   World _world;
   int _old_w = 0;
@@ -24,6 +26,58 @@ class WorldDisplay {
       return;
     }
     drawWorld();
+  }
+
+  private void createWorld(String dirPath) {
+    World new_world = new World(dirPath);
+    if (new_world.isEmpty()) {
+      _applet.println("New World Is Empty");
+      return;
+    }
+
+    _world = new_world;
+    _applet.println("Bounds of New World:", new_world.getBounds());
+  }
+
+  public void folderSelected(File selection) {
+    if (selection == null) {
+      _applet.println("Window was closed or the user hit cancel.");
+    } else {
+      String path = selection.getAbsolutePath();
+      _applet.println("User selected " + path);
+      createWorld(path);
+    }
+  }
+
+  private void setFolder(){
+    _applet.selectFolder("Select a folder to process:", "folderSelected", null, this);
+  }
+
+  public void keyTyped() {
+    System.out.println("typed " + _applet.key + " " + _applet.keyCode);
+    if (_applet.key == 15) { // ctl-o
+      setFolder();
+    }
+    if (_applet.key == 16) {
+      Rectangle bounds = _world.getBounds();
+      int x0 = bounds.x;
+      int x1 = bounds.width - x0;
+      for (int x = x0; x < x1; x++) {
+        int z0 = bounds.y;
+        int z1 = bounds.height - z0;
+        for (int z = z0; z < z1; z++) {
+          Region r = _world.getRegion(x, z);
+          if (r == null) {
+            continue;
+          }
+          for (int cx = 0; cx < 32; cx++) {
+            for (int cz = 0; cz < 32; cz++) {
+              r.chunk_exists(cx, cz);
+            }
+          }
+        }
+      }
+    }
   }
 
   private void handleResize() {
--- WorldMapper/WorldMapper.pde
+++ WorldMapper/WorldMapper.pde
@@ -1,21 +1,5 @@
-import java.lang.Object;
-import java.awt.event.*;
-import java.awt.Rectangle;
+// Copyright 2015, Mark Dyer
 
-
-
-void createWorld(String dirPath) {
-  World new_world = new World(dirPath);
-  if (new_world.isEmpty()) {
-    println("New World Is Empty");
-    return;
-  }
-  
-  world = new_world;
-  println("Bounds of New World:", new_world.getBounds());
-}
-
-World world = null;
 WorldDisplay world_display = null;
 void setup(){
   size (1024, 960);
@@ -26,45 +10,8 @@ void setup(){
   world_display.setup();
 }
 
-void setFolder(){
-  selectFolder("Select a folder to process:", "folderSelected");
-}
-
 void keyTyped() {
-  println("typed " + int(key) + " " + keyCode);
-  if (int(key) == 15) { // ctl-o
-    setFolder();
-  }
-  if (int(key) == 16) {
-    Rectangle bounds = world.getBounds();
-    int x0 = bounds.x;
-    int x1 = bounds.width - x0;
-    for (int x = x0; x < x1; x++) {
-      int z0 = bounds.y;
-      int z1 = bounds.height - z0;
-      for (int z = z0; z < z1; z++) {
-        Region r = world.getRegion(x, z);
-        if (r == null) {
-          continue;
-        }
-        for (int cx = 0; cx < 32; cx++) {
-          for (int cz = 0; cz < 32; cz++) {
-            r.chunk_exists(cx, cz);
-          }
-        }
-      }
-    }
-  }
-}
-
-void folderSelected(File selection) {
-  if (selection == null) {
-    println("Window was closed or the user hit cancel.");
-  } else { 
-    String path = selection.getAbsolutePath();
-    println("User selected " + path);
-    createWorld(path);
-  }
+  world_display.keyTyped();
 }
 
 void draw() {
